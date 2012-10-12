11f23ad101ce9fd3dc14bd493d7010fa469e1fb3
togusav <togusav@linux-1892.site>
2012-10-11 20:58:48 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -1,18 +1,16 @@
 from distutils.core import setup
+from django_pgarray import __version__
 
 
 setup(
     name="django_pgarray",
-    version=__import__("django_pgarray").__version__,
+    version=__version__,
     author="Gustavo Diaz Jaimes",
     author_email="gustavodiazjaimes@gmail.com",
     description="array field for postgres, using csv for widget and serialization",
     long_description=open("README.rst").read(),
     url="https://github.com/gustavodiazjaimes/django_pgarray",
-    packages="django_pgarray",
-    install_requires = [
-        "python-unicodecsv>=0.9",
-    ],
+    packages=["django_pgarray",],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
